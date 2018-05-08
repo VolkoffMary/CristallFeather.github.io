633d1de16983b023797e88110715c52d44c026c9
CristallFeather <volkoffmary@gmail.com>
2018-05-08 17:31:39 +0300

    Starting a NewLife

---
+++ EditForm.js
@@ -0,0 +1,36 @@
+﻿var xhr = new XMLHttpRequest();
+var link = window.location.href;
+var mark = link.indexOf("?id")+4;
+var id = link.substring(mark,link.length);
+var hrefLink='https://swapi.co/api/people/' + id;
+var aHref = 'FormView.html?id=' + id;   
+document.getElementById('returnLink').setAttribute('href', aHref);
+xhr.open('GET', hrefLink, true);
+
+
+xhr.send();
+
+
+
+xhr.onreadystatechange = function() {
+    if (xhr.readyState != 4) return;
+
+    if (xhr.status != 200) {
+    // обработать ошибку
+    alert(xhr.status + ': ' + xhr.statusText);
+    } else {
+    // вывести результат
+    var CRUD = JSON.parse(xhr.responseText);
+
+    var title = document.getElementsByTagName('title')[0];
+    title.innerHTML = CRUD.name;
+
+    var fullName = document.getElementById('name');  
+    fullName.setAttribute('value', CRUD.name);
+
+    var dateOfBirth = document.getElementById('dateOfBirth');
+    const birthday = new Date(CRUD.created);
+    dateOfBirth.setAttribute('value', birthday.toLocaleString());
+    }
+
+}
---
+++ TemplateMain.js
@@ -0,0 +1,30 @@
+﻿var header = document.getElementsByTagName('header')[0];
+var footer = document.getElementsByTagName('footer')[0];
+
+var logo = document.createElement('a');
+var nav = document.createElement('div');
+var patientList = document.createElement('a');
+var emptySpace = document.createElement('div');
+var signIn = document.createElement('a');
+var logoImg = document.createElement('img');
+
+header.appendChild(logo);
+header.appendChild(nav);
+nav.appendChild(patientList);
+nav.appendChild(emptySpace);
+nav.appendChild(signIn);
+
+patientList.innerHTML = 'Пациенты';
+signIn.innerHTML = 'Войти';
+nav.setAttribute('id','nav');
+logo.setAttribute('href', 'MainPage.html');
+patientList.setAttribute('href', 'PatientList.html');
+emptySpace.setAttribute('class', 'emptyspace');
+patientList.setAttribute('class', 'nav-links');
+signIn.setAttribute('class', 'nav-links');
+
+logoImg.setAttribute('src','logo.png');
+logoImg.setAttribute('height','64px');
+logoImg.setAttribute('alt',"I'mabadprogrammer");
+logo.setAttribute('id','logoLink');
+logo.appendChild(logoImg);
