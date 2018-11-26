74c31fcb0075d665ec349aef2b1337c37849dcdf
CristallFeather <volkoffmary@gmail.com>
2018-11-27 00:42:28 +0200

    Update TemplateMain.js

--- public/TemplateMain.js
+++ public/TemplateMain.js
@@ -4,7 +4,6 @@ var footer = document.getElementsByTagName('footer')[0];
 var logo = document.createElement('a');
 var nav = document.createElement('div');
 var emptySpace = document.createElement('div');
-var logoTitle = document.createElement('h1');
 
 header.appendChild(logo);
 header.appendChild(nav);
@@ -13,5 +12,3 @@ nav.appendChild(emptySpace);
 nav.setAttribute('id','nav');
 logo.setAttribute('href', 'MainPage.html');
 emptySpace.setAttribute('class', 'emptyspace');
-
-logo.appendChild(logoTitle);
