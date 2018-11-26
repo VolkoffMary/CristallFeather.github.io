588e024c16f72955ab5fdeda706d532b76b2143f
CristallFeather <volkoffmary@gmail.com>
2018-11-27 00:17:43 +0200

    Update TemplateMain.js

--- public/TemplateMain.js
+++ public/TemplateMain.js
@@ -3,30 +3,16 @@ var footer = document.getElementsByTagName('footer')[0];
 
 var logo = document.createElement('a');
 var nav = document.createElement('div');
-var patientList = document.createElement('a');
-var doctorsList = document.createElement('a');
 var emptySpace = document.createElement('div');
-var signIn = document.createElement('a');
 var logoImg = document.createElement('img');
 
 header.appendChild(logo);
 header.appendChild(nav);
-nav.appendChild(patientList);
-nav.appendChild(doctorsList);
 nav.appendChild(emptySpace);
-nav.appendChild(signIn);
 
-patientList.innerHTML = 'Пациенты';
-doctorsList.innerHTML = 'Доктора';
-signIn.innerHTML = 'Администратор';
 nav.setAttribute('id','nav');
 logo.setAttribute('href', 'MainPage.html');
-patientList.setAttribute('href', 'PatientList.html');
-doctorsList.setAttribute('href', 'DoctorsList.html');
 emptySpace.setAttribute('class', 'emptyspace');
-patientList.setAttribute('class', 'nav-links');
-doctorsList.setAttribute('class', 'nav-links');
-signIn.setAttribute('class', 'nav-links');
 
 logoImg.setAttribute('src','logo.png');
 logoImg.setAttribute('height','64px');
